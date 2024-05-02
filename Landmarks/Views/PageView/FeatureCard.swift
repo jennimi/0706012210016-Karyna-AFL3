ec7e4af7301a17d586dbbd812cb6ab597daeaf9e
Karyna <karyna.sanjaya@gmail.com>
2024-05-02 12:41:00 +0700

    Chapter 4: Framework integration; Interfacing with UIKit

---
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by student on 02/05/24.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
---
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by student on 02/05/24.
+//
+
+import Foundation
