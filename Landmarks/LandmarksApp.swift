37a171e8cdba0a97ce4fc2df8159f403320b4fb5
Karyna <karyna.sanjaya@gmail.com>
2024-05-02 15:41:23 +0700

    Chapter 4: Framework integration; Creating a macOS app

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -17,8 +17,20 @@ struct LandmarksApp: App {
                 .environment(modelData)
         }
         
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
+        
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+        
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
         #endif
     }
     
--- WatchLandmarks Watch App/LandmarkList.swift
+++ WatchLandmarks Watch App/LandmarkList.swift
@@ -1,18 +1,46 @@
 //
 //  LandmarkList.swift
-//  WatchLandmarks Watch App
+//  Landmarks
 //
-//  Created by student on 02/05/24.
+//  Created by student on 18/04/24.
 //
 
 import SwiftUI
 
 struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List{
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
     }
 }
 
 #Preview {
     LandmarkList()
+        .environment(ModelData())
 }
