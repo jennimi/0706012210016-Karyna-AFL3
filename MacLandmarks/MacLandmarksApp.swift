636dd028078bd9ed34afc9fa21dc3746fb6e178c
Karyna <karyna.sanjaya@gmail.com>
2024-05-02 15:34:16 +0700

    Chapter 4: Framework integration; Creating a macOS app

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by student on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
